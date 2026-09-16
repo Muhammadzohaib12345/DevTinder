59b5cb2774a9d9df76bc6ad43ef8d9c64c020ea2
Muhammad Zohaib <stolidkha@gmail.com>
2026-09-16 12:30:01 +0500

    get apis

--- src/app.js
+++ src/app.js
@@ -17,6 +17,63 @@ try{
 }
 })
 
+// Getting users
+app.get("/user", async(req, res)=>{
+try{
+    const email = req.body.email
+    const user = await User.find({email})
+    if(user.length === 0){
+res.status(404).send("User not found")
+    }else{
+        res.send(user)
+    }
+}catch(error){
+res.send(400).send("Something went wrong")
+}
+})
+
+// Getting user by findOne method
+// app.get("/user", async(req, res)=>{
+// try{
+//     const email = req.body.email
+//     const user = await User.findOne()
+//     if(!user){
+// res.status(404).send("User not found")
+//     }else{
+//         res.send(user)
+//     }
+// }catch(error){
+// res.send(400).send("Something went wrong")
+// }
+// })
+
+
+// Getting user by using the FindbyId
+app.get("/findbyid", async(req, res)=>{
+    try{
+        const id = req.body.id
+        const user = await User.findById(id)
+        if(user.length === 0){
+            res.status(400).send("user not found with this id")
+        }else {
+            res.send(user)
+        }
+    }catch(error){
+        res.send("something went wrong")
+    }
+})
+
+
+// Getting the feed
+app.get("/feed", async(req, res)=>{
+    try{
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch(error){
+        res.status(400).send("something went wrong")
+    }
+})
 connectDb() 
 .then(()=>{
     console.log("connection established successfully")
