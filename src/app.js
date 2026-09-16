cfa3e32903e92241261e8f6aa979a5912569f07f
Muhammad Zohaib <stolidkha@gmail.com>
2026-09-16 14:00:57 +0500

    Play with some function of mongoose

--- src/app.js
+++ src/app.js
@@ -52,18 +52,69 @@ res.send(400).send("Something went wrong")
 app.get("/findbyid", async(req, res)=>{
     try{
         const id = req.body.id
+        console.log(id)
         const user = await User.findById(id)
-        if(user.length === 0){
-            res.status(400).send("user not found with this id")
+        if(!user){
+            res.status(404).send("user not found with this id")
         }else {
             res.send(user)
         }
     }catch(error){
-        res.send("something went wrong")
+        if(error.name === "CastError"){
+            res.status(404).send("user not found with this id")
+        }else {
+             res.send("something went wrong")
+        }
     }
 })
 
 
+// Delete user api
+app.delete("/user", async(req, res)=>{
+    try{
+        const id = req.body.id
+        const user = await User.findByIdAndDelete(id)
+if(!user){
+    res.status(404).send("User doesn't found by this id")
+}else{
+  res.send(
+            "User deleted successfully"
+        )
+}  
+    }catch(error){
+if(error.name==="CastError"){
+    res.status(404).send("User not found")
+}else{
+   res.status(500).send("something went wrong") 
+}
+    }
+})
+
+// Update User with id
+app.patch("/user", async(req, res)=>{
+    try{
+        const id = req.body.id
+        const data = req.body
+        const user = await User.findByIdAndUpdate({_id: id}, data, {strict: false})
+        console.log(user)
+        res.send("User data updated successfully")
+    }catch(error){
+        res.status(500).send("Something went wrong")
+    }
+})
+
+
+// update user with email
+app.patch("/email", async(req, res)=>{
+    try{
+        const email = req.body.email
+        const data = req.body
+        const user = await User.findOneAndUpdate({email: email}, data, {strict: false})
+        res.send("user data with the provided email is successfully updated")
+    }catch(error){
+        res.status(500).send("Something went wrong")
+    }
+})
 // Getting the feed
 app.get("/feed", async(req, res)=>{
     try{
